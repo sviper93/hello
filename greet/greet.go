0682b0701c8f5a6b635d85a4a946c055a34b357c
sviper93 <67016337+sviper93@users.noreply.github.com>
2023-02-20 11:57:22 -0500

    Initial commit

---
+++ greet/greet.go
@@ -0,0 +1,11 @@
+package greet
+
+var emoji = "🙋‍♂️"
+
+func English() string {
+	return "Hi " + emoji
+}
+
+func Italian() string {
+	return "Ciao " + emoji
+}
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+}
+
+/*
+
+Creamos un nuevo repositorio dentro de Git con el nombre "hello", luego ejecutamos los
+siguientes comandos para subir el proyecto a Github:
+
+git init
+git add .
+git commit
+Initial commit
+esc + :wq
+git remote add origin https://github.com/sviper93/hello.git -> codigo copiado de "... or push an existing"
+git remote -v
+git push -u origin main
+*/
